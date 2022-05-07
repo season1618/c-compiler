989638064bc4fa1d8331244593f66bdde54301b1
season1618 <seasonminority314@gmail.com>
2022-05-07 22:39:02 +0900

    let 'p' be a global variable.

--- tokenize.c
+++ tokenize.c
@@ -5,17 +5,21 @@
 
 #include "dcl.h"
 
+char *p;
 int NUM_KEYWORD = 5;
 int NUM_PUNCT = 18;
 char *keywords[] = {"return", "if", "else", "while", "for"};
 char *puncts[] = {"==", "!=", "<=", ">=", "=", "+", "-", "*", "/", ";", "<", ">", "(", ")", "{", "}", "[", "]"};
 
-token *next_token(token_kind kind, token *cur, char *p, int len){
+token *next_token(token_kind kind, token *cur, int len){
     token *nxt = calloc(1, sizeof(token));
     nxt->kind = kind;
     nxt->str = p;
     nxt->len = len;
+
     cur->next = nxt;
+    p += len;
+
     return nxt;
 }
 
@@ -31,7 +35,7 @@ bool fwdmatch(char *s, char *t){
     return memcmp(s, t, strlen(t)) == 0;
 }
 
-int len_id(char *p){
+int len_id(){
     int len = 0;
     while(isalnum_(*p)){
         p++;
@@ -40,7 +44,9 @@ int len_id(char *p){
     return len;
 }
 
-token *tokenize(char *p){
+token *tokenize(char *code_head){
+    p = code_head;
+
     token *head = calloc(1, sizeof(token));
     head->next = NULL;
     token *cur = head;
@@ -53,8 +59,7 @@ token *tokenize(char *p){
         }
         for(int i = 0; i < NUM_KEYWORD; i++){
             if(fwdmatch(p, keywords[i]) && !isalnum_(p[strlen(keywords[i])])){
-                cur = next_token(TK_KEYWORD, cur, p, strlen(keywords[i]));
-                p += strlen(keywords[i]);
+                cur = next_token(TK_KEYWORD, cur, strlen(keywords[i]));
                 flag = true;
                 break;
             }
@@ -62,30 +67,28 @@ token *tokenize(char *p){
         if(flag) continue;
         for(int i = 0; i < NUM_PUNCT; i++){
             if(fwdmatch(p, puncts[i])){
-                cur = next_token(TK_PUNCT, cur, p, strlen(puncts[i]));
-                p += strlen(puncts[i]);
+                cur = next_token(TK_PUNCT, cur, strlen(puncts[i]));
                 flag = true;
                 break;
             }
         }
         if(flag) continue;
         if(isdigit(*p)){
-            cur = next_token(TK_NUM, cur, p, 0);
+            cur = next_token(TK_NUM, cur, 0);
             char *q = p;
             cur->val = strtol(p, &p, 10);
             cur->len = p - q;
             continue;
         }
         if(isalpha_(*p)){
-            cur = next_token(TK_ID, cur, p, 0);
-            cur->len = len_id(p);
-            p += cur->len;
+            cur = next_token(TK_ID, cur, 0);
+            cur->len = len_id();
             continue;
         }
         
         error(cur, "invalid token");
     }
-    next_token(TK_EOF, cur, p, 0);
+    next_token(TK_EOF, cur, 0);
     // cur = head->next;
     // while(cur->kind != TK_EOF){
     //     for(int i = 0; i < cur->len; i++){
